2bed3873465128c0e90e31fba93ea9a88f8021ed
josemorista <josemorista@id.uff.br>
2022-10-09 19:21:36 -0300

    feat: added NotifyMembersObserver

--- src/__tests__/integration/PlaceOrder.test.js
+++ src/__tests__/integration/PlaceOrder.test.js
@@ -7,6 +7,7 @@ const Client = require('../../domain/entities/Client');
 const PlaceOrderCommand = require('../../application/commands/PlaceOrderCommand');
 const MemCommandBus = require('../../application/bus/MemCommandBus');
 const NotifyMembersObserver = require('../../application/observers/NotifyMembersObserver');
+const MemMembersRepository = require('../../application/infra/mem/repositories/MemMembersRepository');
 
 const input = {
 	clientName: 'client',
@@ -29,8 +30,8 @@ const expectedOrder = new Order(
 describe('Place Order-integration', () => {
 	it('Should call registered observers with correct payload', async () => {
 		const commandBus = new MemCommandBus();
-		//const membersRepository = new MemMembersRepository();
-		const notifyMembersObserver = new NotifyMembersObserver(/*(membersRepository*/);
+		const membersRepository = new MemMembersRepository();
+		const notifyMembersObserver = new NotifyMembersObserver(membersRepository);
 		notifyMembersObserver.notify = jest.fn();
 		commandBus.subscribe(notifyMembersObserver);
 		const command = new PlaceOrderCommand(input);
--- src/application/observers/NotifyMembersObserver.js
+++ src/application/observers/NotifyMembersObserver.js
@@ -1,6 +1,5 @@
 'use strict';
-
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const CommandObserver = require('./CommandObserver');
 const { COMMANDS } = require('../../constants');
 
@@ -14,11 +13,72 @@ module.exports = class NotifyMembersObserver extends CommandObserver {
 		return COMMANDS.PLACE_ORDER;
 	}
 
-	getParameters(order, member) {}
+	#getParameters(order, member) {
+		return [
+			{
+				type: 'text',
+				text: member.name,
+			},
+			{
+				type: 'text',
+				text: order.client.name,
+			},
+			{
+				type: 'text',
+				text: order.client.enterprise || '',
+			},
+			{
+				type: 'text',
+				text: order.services.join(','),
+			},
+			{
+				type: 'text',
+				text: order.duration || '',
+			},
+			{
+				type: 'text',
+				text: order.reference || '',
+			},
+			{
+				type: 'text',
+				text: order.client.contactPhone,
+			},
+			{
+				type: 'text',
+				text: order.briefing || '',
+			},
+		];
+	}
 
 	async notify(command) {
 		const order = await command.getPayload();
 		const members = await this.membersRepository.all();
-		Promise.all(members.map(member => fetch()));
+		Promise.allSettled(
+			members.map(member =>
+				fetch(`https://graph.facebook.com/v14.0/${process.env.WHATSAPP_SENDER_ID}/messages`, {
+					method: 'POST',
+					body: JSON.stringify({
+						messaging_product: 'whatsapp',
+						to: member.phone,
+						type: 'template',
+						template: {
+							name: 'alerta_msg_form',
+							language: {
+								code: 'pt_BR',
+							},
+							components: [
+								{
+									type: 'body',
+									parameters: this.#getParameters(order, member),
+								},
+							],
+						},
+					}),
+					headers: {
+						'Content-type': 'application/json',
+					},
+				})
+			)
+		);
 	}
 };
